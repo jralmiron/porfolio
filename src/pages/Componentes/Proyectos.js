e30f1288977a5ee678d5681480009c79c3f8ac2e
juan almi <jr_almi@hotmail.com>
2025-02-17 21:22:14 +0100

    comit aumento escala

--- src/pages/Componentes/Proyectos.js
+++ src/pages/Componentes/Proyectos.js
@@ -6,7 +6,7 @@ const Proyectos = () => (
       <h2 className="text-2xl mb-12 fade-in text-primary">Proyectos Seleccionados</h2>
       <div className="space-y-16">
         {["Proyecto 01", "Proyecto 02"].map((proyecto, index) => (
-          <article key={index} className="group fade-in hover:scale-[1.02] transition-all duration-300">
+          <article key={index} className="group fade-in hover:scale-[1.50] transition-all duration-300">
             <a href="#" className="block">
               <div className="aspect-video bg-secondary mb-4 rounded-lg shadow-lg transform transition-transform"></div>
               <h3 className="text-xl group-hover:underline text-primary">{proyecto}</h3>
